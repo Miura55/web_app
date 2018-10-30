d1b5b8dfc8ce4223e48121191a5851c61866e536
Miura55 <acordion.piano@gmail.com>
2018-10-30 11:49:02 +0900

    Fix Checkbox

--- check_test/app.py
+++ check_test/app.py
@@ -1,26 +1,16 @@
-import os
-import sqlite3
-from flask import Flask, render_template, abort, request
+from flask import Flask,render_template,url_for,request,redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-# GETメソッドの場合
-@app.route("/test1")
-def test1():
-    favs = request.args.getlist("fav")
-    print("favs:", favs) # ['1','2','3']
+@app.route('/')
+def hello():
+    return render_template('test.html')
+
+@app.route('/posttest',methods = ['POST'])
+def posttest():
+    print(request.form.getlist('ch'))
     return "ok"
 
-# POSTメソッドの場合
-@app.route("/test2", methods=['POST'])
-def test2():
-    favs = request.form.getlist("fav")
-    print("favs:", favs) # ['1','2','3']
-    return "ok"
 
 if __name__ == '__main__':
-    app.debug = True # デバッグモード有効化
-    app.run(host='0.0.0.0') # どこからでもアクセス可能に
+    app.run(debug=True)
